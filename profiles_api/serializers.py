637cacd6452723169318bd4930aa212791943dea
SharifSayyad <sayyadshairf164@gmail.com>
2022-11-29 14:11:47 +0530

    Added profiles viewset

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,5 +1,38 @@
 from rest_framework import serializers
+from profiles_api import models
 
 class HelloSerializer(serializers.Serializer):
     """ Serializers a name field for testing our APIView """
     name = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializer a user profile object"""
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        # fields = '__all__' to include all fields
+        # exclude = ('id') to exclued some fields
+        extra_kwargs = {
+            'password':{
+                'write_only':True,
+                'style':{'input_type':'password'}
+            }
+        }# for set password only write not for retrive
+    def create(self, validated_data):
+        """ create method override because need to store password encripted """
+        user = models.UserProfile.objects.create_user(
+            email = validated_data['email'],
+            name = validated_data['name'],
+            password = validated_data['password']
+        )
+        return user
+
+    def update(self, instance, validated_data):
+        """Handle updating user account"""
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+
+        return super().update(instance, validated_data)
